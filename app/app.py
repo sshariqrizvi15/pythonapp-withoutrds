1e6d359d506bcb7f37844dec1600d4fa2519cf57
Shariq Rizvi <34273209+sshariqrizvi15@users.noreply.github.com>
2019-07-10 02:25:14 +0500

    welcome 104

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome103!"
+    return "Welcome104!"
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug = False)
